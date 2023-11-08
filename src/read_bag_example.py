a432b609e199efc80f7111ed9361561d0253d342
Jeffrey Lee <jeffrey1218@live.com>
2023-11-07 23:23:48 -0500

    cleanup and add file selectors for bag processing scripts

--- src/read_bag_example.py
+++ src/read_bag_example.py
@@ -1,40 +1,10 @@
-# Source: https://github.com/IntelRealSense/librealsense/blob/master/wrappers/python/examples/read_bag_example.py
+# Modified from: https://github.com/IntelRealSense/librealsense/blob/master/wrappers/python/examples/read_bag_example.py
 
-#####################################################
-##               Read bag from file                ##
-#####################################################
-
-
-# First import library
 import pyrealsense2 as rs
-# Import Numpy for easy array manipulation
 import numpy as np
-# Import OpenCV for easy image rendering
 import cv2
-# Import argparse for command-line options
-import argparse
-# Import os.path for file path manipulation
 import os.path
-
 from tkinter import filedialog as fd
-
-# # Create object for parsing command-line options
-# parser = argparse.ArgumentParser(description="Read recorded bag file and display depth stream in jet colormap.\
-#                                 Remember to change the stream fps and format to match the recorded.")
-# # Add argument which takes path to a bag file as an input
-# parser.add_argument("-i", "--input", type=str, help="Path to the bag file")
-# # Parse the command line arguments to an object
-# args = parser.parse_args()
-# # Safety if no parameter have been given
-# if not args.input:
-#     print("No input paramater have been given.")
-#     print("For help type --help")
-#     exit()
-# # Check if the given file have bag extension
-# if os.path.splitext(args.input)[1] != ".bag":
-#     print("The given file is not of correct file format.")
-#     print("Only .bag files are accepted")
-#     exit()
 
 path = os.path.dirname(os.path.abspath(__file__))
 
@@ -48,15 +18,12 @@ try:
     filename = fd.askopenfilename(initialdir=path + "/bags/")
 
     # Tell config that we will use a recorded device from file to be used by the pipeline through playback.
-    # rs.config.enable_device_from_file(config, args.input)
-    # rs.config.enable_device_from_file(config, path + "/bags/2023-10-30 20:41:50.807534 - cam0.bag")
     rs.config.enable_device_from_file(config, filename)
 
 
     # Configure the pipeline to stream the depth stream
     # Change this parameters according to the recorded bag file resolution
     config.enable_stream(rs.stream.depth, rs.format.z16, 30)
-    
     config.enable_stream(rs.stream.color, rs.format.bgr8, 30)
 
     # Start streaming from file
